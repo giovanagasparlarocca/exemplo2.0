e395a53caa45d9ff76f7ee2c07224059a01b2591
Giovana <giovanalarocca07@gmail.com>
2025-03-28 11:42:02 -0300

    declaração de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+//alert("Hello world");
+//declaração e variaveis
+
+var nome="Fiap";
+console.log(nome)
+
+let idade="17";
+console.log(idade)
+
+const sobrenome= "Fiapinho";
+console.log(sobrenome)
+
+//undefine
+let exemplo;
+console.log(exemplo)
+
+//nula
+let exemplo1= null;
+console.log(exemplo1)
+

8e285c9af737379a8b9014ea19185c76a59662d9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-04 11:42:19 -0300

    adicionando uma comparação

--- src/js/script.js
+++ src/js/script.js
@@ -65,3 +65,25 @@ let info ="programação de ponta"
 console.log(info.slice(15,20))
 
 // operadores aritimeticos
+const a=10;
+const b=20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+//operadores logicos
+const c=20;
+const d=30;
+console.log(c < d);
+console.log(c> d && c<10);
+console.log(c > b || d > c);
+console.log( c == d || d <= c);
+
+// operdores de comparação
+const e=10;
+const f=30;
+
+console.log(e==f);
+console.log(e===f);
+console.log(e!=f);

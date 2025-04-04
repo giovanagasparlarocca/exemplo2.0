013de21f4a41decc721e027a5d7caaa631e13be4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-04 11:03:05 -0300

    adicionando metodos

--- src/js/script.js
+++ src/js/script.js
@@ -50,3 +50,18 @@ console.log(numInt.toString())
 // string pra int
 let numString1= "12"
 console.log(parseInt(numString1))
+
+// metodos
+//metodo length- verifica tamanho da string
+let frase = "o mundo da tecnologia"
+console.log(frase.length)
+
+// metodo indexof/lastindexof- retorna um trecho do seu codigo
+let texto = "programação sustentável"
+console.log(texto.indexOf("ão"))
+
+// metodo slice- retorna parte de um texto passando o inicio e o final
+let info ="programação de ponta"
+console.log(info.slice(15,20))
+
+// operadores aritimeticos
